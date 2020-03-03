ad3a59cad109036059fe2ffd63820ae1af938295
Scott Smith <stsmith@callutheran.edu>
2020-03-03 13:42:42 -0800

    added react-snapshot

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
+
 import App from './App';
 import { StoreProvider } from './Store';
 
 import './scss/styles.scss';
 
 ReactDOM.render(
-	<StoreProvider>
-		<App />
-	</StoreProvider>,
-	document.getElementById("root")
+	render(
+		<StoreProvider>
+			<App />
+		</StoreProvider>,
+		document.getElementById("root")
+	)
 );
